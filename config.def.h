ed553898926ad87541def4d66d0a874ed49131bc
r4tss <68610647+r4tss@users.noreply.github.com>
2021-02-03 10:15:25 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -9,12 +9,11 @@ static const unsigned int gappiv    = 0;       /* vert inner gap between windows
 static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int splitstatus	    = 1;
-static const char *splitdelim	    = "";
+static const char *splitdelim	    = ";";
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "cozette:size=10" };
-static const char dmenufont[]       = "cozette:size=10";
+static const char *fonts[]          = { "Cozette:size=12", "Siji:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -26,11 +25,11 @@ static const char col_white[]	    = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_white, col_gray5, col_gray5 },
-	[SchemeSel]  = { col_white, col_acc,   col_acc   },
+	[SchemeSel]  = { col_gray5, col_acc,   col_acc   },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "\ue00e", "\ue1a0", "\ue1ec", "\ue1e5", "\ue19f" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,10 +38,13 @@ static const Rule rules[] = {
 	 */
 
 	       /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+		{ "lightcord",	NULL,	NULL,		1 << 4,		0,	0,		1,	-1},
+		{ "discord",	NULL,	NULL,		1 << 4,		0,	0,		1,	-1},
+		{ "surf",	NULL,	NULL,		1 << 1,		0,	0,		1,	-1},
+		{ "LibreWolf",	NULL,	NULL,		1 << 1,		0,	0,		1,	-1},
 		{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 		{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-		{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-		{ "sdl",     NULL,     "sdl",	       0,	  1,	      0,           1,        -1 },
+		{ "st",      NULL,     NULL,           1 << 2,    0,          1,           0,        -1 },
 		{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -73,30 +75,35 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray5, "-nf", col_white, "-sb", col_acc, "-sf", col_gray5, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-p", "Select an application to run", "-m", dmenumon, "-nb", col_gray5, "-nf", col_white, "-sb", col_acc, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g" "150x40", NULL };
 
 static Key keys[] = {
 	/* modifier                     key       	function        	argument */
 	{ MODKEY,                       XK_p,		spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, 	spawn,          {.v = termcmd } },
+	{ MODKEY,			XK_grave,	togglescratch,	{.v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,		XK_s,	   	spawn,	   	SHCMD("maim -su | xclip -selection clipboard -t image/png -i") },
 	{ MODKEY,			XK_b,	   	spawn,	   	SHCMD("librewolf") },
-	{ MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("discord") },
+	{ MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("lightcord") },
 	{ MODKEY,			XK_n,		spawn,		SHCMD("st -e ncmpcpp") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD("lutris") },
 	{ MODKEY,			XK_Insert,	spawn,		SHCMD("sn") },
 	{ MODKEY,			XK_End,		spawn,		SHCMD("xsecurelock") },
-	{ MODKEY,			XK_F5,		spawn,		SHCMD("$HOME/scripts/musicBlock.sh set clear") },
-	{ MODKEY,			XK_F6,		spawn,		SHCMD("$HOME/scripts/musicBlock.sh set prev") },
-	{ MODKEY,			XK_F7,		spawn,		SHCMD("$HOME/scripts/musicBlock.sh set toggle") },
-	{ MODKEY,			XK_F8,		spawn,		SHCMD("$HOME/scripts/musicBlock.sh set next") },
-	{ MODKEY,			XK_F10,		spawn,		SHCMD("$HOME/scripts/cVolume.sh down") },
-	{ MODKEY,			XK_F11,		spawn,		SHCMD("$HOME/scripts/cVolume.sh up") },
+	{ MODKEY,			XK_F5,		spawn,		SHCMD("$HOME/.scripts/musicBlock.sh set clear") },
+	{ MODKEY,			XK_F6,		spawn,		SHCMD("$HOME/.scripts/musicBlock.sh set prev") },
+	{ MODKEY,			XK_F7,		spawn,		SHCMD("$HOME/.scripts/musicBlock.sh set toggle") },
+	{ MODKEY,			XK_F8,		spawn,		SHCMD("$HOME/.scripts/musicBlock.sh set next") },
+	{ MODKEY,			XK_F10,		spawn,		SHCMD("$HOME/.scripts/cVolume.sh down") },
+	{ MODKEY,			XK_F11,		spawn,		SHCMD("$HOME/.scripts/cVolume.sh up") },
 	{ MODKEY|ShiftMask,		XK_F10,		spawn,		SHCMD("mpc volume -5") },
 	{ MODKEY|ShiftMask,		XK_F11,		spawn,		SHCMD("mpc volume +5") },
-	{ MODKEY|ShiftMask,		XK_p,		spawn,		SHCMD("$HOME/scripts/setlayout.sh") },
-	{ MODKEY,			XK_m,		spawn,		SHCMD("$HOME/scripts/dmenumpd.sh") },
+	{ MODKEY|ShiftMask,		XK_p,		spawn,		SHCMD("$HOME/.scripts/setlayout.sh") },
+	{ MODKEY,			XK_m,		spawn,		SHCMD("$HOME/.scripts/dmenumpd.sh") },
+	{ MODKEY|ShiftMask,		XK_Up,		spawn,		SHCMD("$HOME/.scripts/timer.sh reset") },
+	{ MODKEY,			XK_equal,	spawn,		SHCMD("$HOME/.scripts/alarm.sh") },
 	{ MODKEY|ShiftMask|ControlMask, XK_b,      	togglebar,     	{0}},
 	{ MODKEY,			XK_j,	   	focusstack,	{.i = +1 } },
 	{ MODKEY,			XK_k,	   	focusstack,	{.i = -1 } },
